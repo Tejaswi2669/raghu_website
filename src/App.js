9417cb6a0c7db181309ad3ec55737294c0b73dbf
Tejaswi2669 <tejaswinerella9@gmail.com>
2025-03-24 15:24:38 +0100

    Reveretd to old app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter, Routes, Route } from 'react-router-dom';
+import {  Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './components/HomePage';
@@ -10,11 +10,12 @@ import './styles/App.css';
 
 const App = () => {
     return (
-        <HashRouter>
+       
             <div id="root">
                 <Header />
                 <main>
                     <Routes>
+                        <Route index element={<Home />} />
                         <Route index element={<Home />} />
                         <Route path="/home" element={<Home />} />
                         <Route path="/about" element={<About />} />
@@ -24,7 +25,7 @@ const App = () => {
                 </main>
                 <Footer />
             </div>
-        </HashRouter>
+        
     );
 };
 
